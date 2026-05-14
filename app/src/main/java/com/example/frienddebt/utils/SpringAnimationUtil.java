e2f2b5d6af37b3ff0e578877df528768ca3d805a
monik diwani <monikdiwani@gmail.com>
2026-05-14 21:36:08 +0530

    fix: resolve missing R import in SpringAnimationUtil

--- app/src/main/java/com/example/frienddebt/utils/SpringAnimationUtil.java
+++ app/src/main/java/com/example/frienddebt/utils/SpringAnimationUtil.java
@@ -3,6 +3,9 @@ package com.example.frienddebt.utils;
 import android.annotation.SuppressLint;
 import android.view.MotionEvent;
 import android.view.View;
+
+import com.example.frienddebt.R;
+
 import androidx.dynamicanimation.animation.DynamicAnimation;
 import androidx.dynamicanimation.animation.SpringAnimation;
 import androidx.dynamicanimation.animation.SpringForce;
